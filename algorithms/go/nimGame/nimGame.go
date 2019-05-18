35ee1159029b183b9b1175e4c0207fa26949462c
946328371@qq.com <946328371@qq.com>
2019-05-18 11:47:32 +0800

    add Nim Game solution

---
+++ algorithms/go/nimGame/nimGame.go
@@ -0,0 +1,34 @@
+// Source: https://leetcode.com/problems/nim-game/
+// Author: Egbert11
+// Date: 2019-05-18
+
+/*
+You are playing the following Nim Game with your friend: There is a heap of stones on the table, each time one of you
+take turns to remove 1 to 3 stones. The one who removes the last stone will be the winner. You will take the first
+turn to remove the stones.
+
+Both of you are very clever and have optimal strategies for the game. Write a function to determine whether you can win
+the game given the number of stones in the heap.
+
+Example:
+
+	Input: 4
+	Output: false
+	Explanation: If there are 4 stones in the heap, then you will never win the game;
+				 No matter 1, 2, or 3 stones you remove, the last stone will always be
+				 removed by your friend.
+*/
+
+package main
+
+import "fmt"
+
+func canWinNim(n int) bool {
+	return n % 4 != 0
+}
+
+func main() {
+	for i := 1; i < 12; i++ {
+		fmt.Println(canWinNim(i))
+	}
+}
